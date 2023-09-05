49400290c97018438d6181d889e10d22b902353e
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2023-09-05 11:04:10 -0300

    :arrow_up: iniciando a activity home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 
 dependencies {
 
+    //    CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+    //noinspection GradleDependency
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
